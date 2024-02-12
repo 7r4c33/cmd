c6dcdc9d8cd77f73ed2e8772119c0606c32e80ff
Trace <157802069+7r4c33@users.noreply.github.com>
2024-02-12 14:00:41 +0100

    Create batch.bat

---
+++ batch.bat
@@ -0,0 +1,18 @@
+echo checking server status...
+timeout /t 6
+echo connected!
+timeout /t 3
+cls
+goto x
+
+:x
+echo recived: h:\cdn\ad
+echo send c:\windows\system32
+echo curled: 24 from f:
+echo mkdir:  theres no directory specified (kill)
+echo advanced system care insralled
+echo free chataigpt installed
+echo free avast crack antivairus cer installed
+echo unbloater free crack legit has been installed.
+echo server connection: FREE LEGIT MINECRAFT WITH MODS INSTALLED
+goto x
